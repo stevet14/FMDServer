4855db9fdafca6de55c2f2daca6d13e9cb4e0536
steve <steve@e07742ac-6e3b-4ac5-9087-ca13198b9003>
2010-03-10 09:15:56 +0000

    More tidying

--- FMDServer/WEB-INF/src/org/averni/fmd/SymbolLoader.java
+++ FMDServer/WEB-INF/src/org/averni/fmd/SymbolLoader.java
@@ -1,11 +1,9 @@
 package org.averni.fmd;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
