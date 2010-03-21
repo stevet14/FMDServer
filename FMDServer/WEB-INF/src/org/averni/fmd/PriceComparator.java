c5f2a4724eefe44a4c52484d5a48e9ba19156372
steve <steve@e07742ac-6e3b-4ac5-9087-ca13198b9003>
2010-03-21 23:33:39 +0000

    Regression Analysis complete...

---
+++ FMDServer/WEB-INF/src/org/averni/fmd/PriceComparator.java
@@ -0,0 +1,19 @@
+package org.averni.fmd;
+
+import java.util.Comparator;
+
+import org.averni.fmd.domain.Price;
+
+public class PriceComparator implements Comparator<Price> {
+
+	@Override
+	public int compare(Price price1, Price price2) {
+		if (price1.getDate().before(price2.getDate()))
+			return 1;
+		else if (price1.getDate().after(price2.getDate()))
+			return -1;
+		else
+			return 0;
+	}
+
+}
