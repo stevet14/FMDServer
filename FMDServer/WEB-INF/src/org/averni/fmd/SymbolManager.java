663ba2afff78aeacbec5eddd7e5eaebc4d03811c
steve <steve@e07742ac-6e3b-4ac5-9087-ca13198b9003>
2010-05-01 17:37:17 +0000

    run all symbols

--- FMDServer/WEB-INF/src/org/averni/fmd/SymbolManager.java
+++ FMDServer/WEB-INF/src/org/averni/fmd/SymbolManager.java
@@ -47,9 +47,9 @@ public class SymbolManager implements Job {
 	private void loadAndStoreSymbols() {
 		SymbolLoader sl = new SymbolLoader();
 
-/*		System.out.println("\nExchange: " + Exchange.FINDEX + "\n");
+		System.out.println("\nExchange: " + Exchange.FINDEX + "\n");
 		sl.loadSymbols(Exchange.FINDEX);
-*/		System.out.println("\nExchange: " + Exchange.INDEX + "\n");
+		System.out.println("\nExchange: " + Exchange.INDEX + "\n");
 		sl.loadSymbols(Exchange.INDEX);
 		System.out.println("\nExchange: " + Exchange.LSE + "\n");
 		sl.loadSymbols(Exchange.LSE);
