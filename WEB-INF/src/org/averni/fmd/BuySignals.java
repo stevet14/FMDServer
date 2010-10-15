2b91ae5b077e59b9d10ad003e27417c8b14d54e0
steve <steve@e07742ac-6e3b-4ac5-9087-ca13198b9003>
2010-10-15 12:03:47 +0000

    Added new 'Signals' service to report on this week's buy signals...

--- WEB-INF/src/org/averni/fmd/BuySignals.java
+++ WEB-INF/src/org/averni/fmd/BuySignals.java
@@ -21,156 +21,151 @@ import org.hibernate.criterion.Restrictions;
 import au.com.bytecode.opencsv.CSVWriter;
 
 public class BuySignals {
-	/**
-	 * Logger for this class
-	 */
-	private static final Logger log = Logger.getLogger(BuySignals.class);
+  /**
+   * Logger for this class
+   */
+  private static final Logger log = Logger.getLogger(BuySignals.class);
 
-	private static CSVWriter writer;
-	private static Session session;
+  private static CSVWriter    writer;
+  private static Session      session;
 
-	@SuppressWarnings("unchecked")
-	public static void main(String[] args) throws IOException {
 
-		String buySignals = getBuySignals();
-		log.info(buySignals);
-		if(true) return;
-		
-		session = HibernateUtil.getSessionFactory().getCurrentSession();
-		session.beginTransaction();
-		writer = new CSVWriter(new FileWriter("BuySignals.csv"), ',',
-				CSVWriter.NO_QUOTE_CHARACTER);
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) throws IOException {
 
-		Query q = session
-				.createQuery("from Symbol as symbol where (exchange='LSE' or exchange = 'FUTURES' or exchange = 'FINDEX' or exchange = 'INDEX') order by exchange, symbol")
-				;
-		
-		//Pageing logic (per 1000 results)
-		for(int i=0; i<1000000; i+=1000) {
-			q.setFirstResult(i);
-			q.setMaxResults(1000);
-			q.setFetchSize(1000);
-			List<Symbol> symbols = q.list();
-			if(symbols.size()==0) break; //break once we run out of results...
-			for (Symbol symbol : symbols) {
-				Set<Price> prices = symbol.getPrices();
-				if (prices.size() < 51) {
-					continue;
-				}
+    String buySignals = getBuySignals();
+    log.info(buySignals);
+    if (true) return;
 
-				String breakoutSignal = getBreakoutSignal(prices);
+    session = HibernateUtil.getSessionFactory().getCurrentSession();
+    session.beginTransaction();
+    writer = new CSVWriter(new FileWriter("BuySignals.csv"), ',', CSVWriter.NO_QUOTE_CHARACTER);
 
-				if (breakoutSignal.equals("Buy")) {
-					log.info(symbol.getSymbol() + " - "
-							+ symbol.getDescription() + "\n");
+    Query q = session
+        .createQuery("from Symbol as symbol where (exchange='LSE' or exchange = 'FUTURES' or exchange = 'FINDEX' or exchange = 'INDEX') order by exchange, symbol");
 
-					// CSV Output
-					String[] entries = new String[3];
-					entries[0] = symbol.getSymbol();
-					entries[1] = symbol.getDescription();
-					entries[2] = breakoutSignal;
-					writer.writeNext(entries);					
-				}
-				session.evict(symbol);
-			}
-		session.clear();
-		}
-		writer.close();
-		session.close();
-	}
+    // Pageing logic (per 1000 results)
+    for (int i = 0; i < 1000000; i += 1000) {
+      q.setFirstResult(i);
+      q.setMaxResults(1000);
+      q.setFetchSize(1000);
+      List<Symbol> symbols = q.list();
+      if (symbols.size() == 0) break; // break once we run out of results...
+      for (Symbol symbol : symbols) {
+        Set<Price> prices = symbol.getPrices();
+        if (prices.size() < 51) {
+          continue;
+        }
 
-	private static String getBreakoutSignal(Set<Price> prices) {
-		double current40WeekMA = getMovingAverage(prices, 1, 40);
-		double previous40WeekMA = getMovingAverage(prices, 2, 41);
-		Price price = prices.iterator().next();
-		double close = price.getClose();
-		double high = getPrior12WeekHigh(prices);
-		String breakoutSignal = (close > current40WeekMA)
-				&& (current40WeekMA > previous40WeekMA) && (close > high) ? "Buy"
-				: "Hold";
+        String breakoutSignal = getBreakoutSignal(prices);
 
-		if (breakoutSignal.equals("Buy")) {
-			log.info("Close - " + close + "| Current 40wMA - "
-					+ current40WeekMA + "|  Previous 40wMA - "
-					+ previous40WeekMA);
-			log.info("Breakout Signal: " + breakoutSignal
-					+ "    (Previous 12-wk high: " + high + ")");
-			
-			//Generate signal.
-			//TODO...Need to check for pre-existing signals...
-			Signal signal = new Signal();
-			signal.setSymbol(price.getSymbol());
-			signal.setSignalType("Upside Breakout");
-			signal.setBuyDate(price.getDate());
-			signal.setBuyPrice(price.getClose());
-			session.save(signal);
-			
-			Symbol symbol = price.getSymbol();
-			Set<Signal> signals = symbol.getSignals();
-			if (signals==null) signals = new HashSet<Signal>();
-			signals.add(signal);
-			symbol.setSignals(signals);
-			session.save(symbol);
-			session.flush();
-		}
-		return breakoutSignal;
-	}
+        if (breakoutSignal.equals("Buy")) {
+          log.info(symbol.getSymbol() + " - " + symbol.getDescription() + "\n");
 
-	private static double getMovingAverage(Set<Price> prices, int startWeek,
-			int weeks) {
-		double ma = 0;
-		Iterator<Price> it = prices.iterator();
-		for (int i = 1; i < startWeek; i++) {
-			it.next();
-		}
-		for (int i = startWeek - 1; i < weeks; i++) {
-			Price price = it.next();
-			ma += price.getClose();
-		}
-		return ma / (weeks - startWeek + 1);
-	}
+          // CSV Output
+          String[] entries = new String[3];
+          entries[0] = symbol.getSymbol();
+          entries[1] = symbol.getDescription();
+          entries[2] = breakoutSignal;
+          writer.writeNext(entries);
+        }
+        session.evict(symbol);
+      }
+      session.clear();
+    }
+    writer.close();
+    session.close();
+  }
 
-	private static double getPrior12WeekHigh(Set<Price> prices) {
-		double high = 0;
-		Iterator<Price> it = prices.iterator();
-		it.next(); // skip 'this week'
-		for (int i = 1; i <= 12; i++) {
-			Price price = it.next();
-			if (price.getHigh() > high)
-				high = price.getHigh();
-		}
-		return high;
-	}
-	
-	public static String getBuySignals() {
-		String buySignals = "";
-		
-		//Set scheduled daterange to be last full Monday-to-Friday.
-		Calendar cal = Calendar.getInstance();
-		
-		log.info("Current date: " + cal.getTime());
 
-		if(cal.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY) 
-			cal.add(Calendar.DAY_OF_MONTH, -5);
-		else
-			cal.add(Calendar.DAY_OF_MONTH, -cal.get(Calendar.DAY_OF_WEEK) - 5);
-		
-		log.info("BuySignals as at: " + cal.getTime());
-		
-		session = HibernateUtil.getSessionFactory().getCurrentSession();
-		log.info("BuySignals:\n" + buySignals);
-		session.beginTransaction();
+  private static String getBreakoutSignal(Set<Price> prices) {
+    double current40WeekMA = getMovingAverage(prices, 1, 40);
+    double previous40WeekMA = getMovingAverage(prices, 2, 41);
+    Price price = prices.iterator().next();
+    double close = price.getClose();
+    double high = getPrior12WeekHigh(prices);
+    String breakoutSignal = (close > current40WeekMA) && (current40WeekMA > previous40WeekMA) && (close > high) ? "Buy" : "Hold";
 
-		List<Signal> signals = session.createCriteria(Signal.class)
-			.add( Restrictions.ge("buyDate", cal.getTime()))
-	    	.addOrder( Order.desc("symbol.exchange") )
-	    	.addOrder( Order.asc("symbol.description") )
-	    .list();
-		
-		for(Signal signal : signals) {
-			buySignals.concat(signal.getSymbol().getSymbol() + "," + signal.getSymbol().getDescription()+ "\n");
-		}
-		log.info("BuySignals:\n" + buySignals);
-		return buySignals;
-	}
+    if (breakoutSignal.equals("Buy")) {
+      log.info("Close - " + close + "| Current 40wMA - " + current40WeekMA + "|  Previous 40wMA - " + previous40WeekMA);
+      log.info("Breakout Signal: " + breakoutSignal + "    (Previous 12-wk high: " + high + ")");
+
+      // Generate signal.
+      // TODO...Need to check for pre-existing signals...
+      Signal signal = new Signal();
+      signal.setSymbol(price.getSymbol());
+      signal.setSignalType("Upside Breakout");
+      signal.setBuyDate(price.getDate());
+      signal.setBuyPrice(price.getClose());
+      session.save(signal);
+
+      Symbol symbol = price.getSymbol();
+      Set<Signal> signals = symbol.getSignals();
+      if (signals == null) signals = new HashSet<Signal>();
+      signals.add(signal);
+      symbol.setSignals(signals);
+      session.save(symbol);
+      session.flush();
+    }
+    return breakoutSignal;
+  }
+
+
+  private static double getMovingAverage(Set<Price> prices, int startWeek, int weeks) {
+    double ma = 0;
+    Iterator<Price> it = prices.iterator();
+    for (int i = 1; i < startWeek; i++) {
+      it.next();
+    }
+    for (int i = startWeek - 1; i < weeks; i++) {
+      Price price = it.next();
+      ma += price.getClose();
+    }
+    return ma / (weeks - startWeek + 1);
+  }
+
+
+  private static double getPrior12WeekHigh(Set<Price> prices) {
+    double high = 0;
+    Iterator<Price> it = prices.iterator();
+    it.next(); // skip 'this week'
+    for (int i = 1; i <= 12; i++) {
+      Price price = it.next();
+      if (price.getHigh() > high) high = price.getHigh();
+    }
+    return high;
+  }
+
+
+  public static String getBuySignals() {
+    String buySignals = "";
+
+    // Set scheduled daterange to be last full Monday-to-Friday.
+    Calendar cal = Calendar.getInstance();
+
+    log.info("Current date: " + cal.getTime());
+
+    if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+      cal.add(Calendar.DAY_OF_MONTH, -5);
+    else
+      cal.add(Calendar.DAY_OF_MONTH, -cal.get(Calendar.DAY_OF_WEEK) - 5);
+
+    log.info("BuySignals as at: " + cal.getTime());
+
+    session = HibernateUtil.getSessionFactory().getCurrentSession();
+    session.beginTransaction();
+
+    List<Signal> signals = session.createCriteria(Signal.class)
+      .createAlias("symbol", "symbol")
+      .add(Restrictions.ge("buyDate", cal.getTime()))
+      .addOrder(Order.asc("symbol.exchange"))
+      .addOrder(Order.asc("symbol.description"))
+      .list();
+
+    for (Signal signal : signals) {
+      buySignals = buySignals.concat(signal.getSymbol().getSymbol() + "," + signal.getSymbol().getDescription() + "\n");
+    }
+    log.info("Buy signals found: " + signals.size());
+    return buySignals;
+  }
 }
--- WEB-INF/src/org/averni/fmd/FMDServer.java
+++ WEB-INF/src/org/averni/fmd/FMDServer.java
@@ -2,6 +2,7 @@ package org.averni.fmd;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -10,28 +11,32 @@ import javax.servlet.http.HttpServletResponse;
 
 public class FMDServer extends HttpServlet {
 
-    /**
+  /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	
-	public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws IOException, ServletException
-    {
-        response.setContentType("text/plain");
-        PrintWriter out = response.getWriter();
-        String symbol = request.getParameter("symbol");
-		SymbolLoader sl = new SymbolLoader();
-		String data = sl.getSymbolData(symbol);
-        out.println(data);
-    }
+  private static final long serialVersionUID = 1L;
 
-    public void doPost(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws IOException, ServletException
-    {
-        doGet(request, response);
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    response.setContentType("text/plain");
+    PrintWriter out = response.getWriter();
+    String data = "";
+    Map<String, String> params = request.getParameterMap();
+    if (params.containsKey("symbol")) {
+      String symbol = request.getParameter("symbol");
+      SymbolLoader sl = new SymbolLoader();
+      data = sl.getSymbolData(symbol);
+    } else if (params.containsKey("signals")) {
+      data = BuySignals.getBuySignals(); 
     }
+    out.println(data);
+  }
+
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    doGet(request, response);
+  }
 
 }
